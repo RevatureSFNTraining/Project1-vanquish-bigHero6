45e2e6569d3ab4c8cdd16fa3fc2e8c6de221580d
acgee <acg.bcc@gmail.com>
2022-01-20 07:28:42 -0800

    Delete lwc directory

--- lwc/radioGroupOrig/radioGroupOrig.js
+++
@@ -1,9 +0,0 @@
-import {LightningElement, track} from 'lwc';
-export default class RadioButtonGroup extends LightningElement{
-   radioName;
-    
-    handleButtonChoice(event){
-        this.radioName = event.target.value;        
-    }
-
-}

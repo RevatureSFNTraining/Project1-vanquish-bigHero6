f2196ace850af685ac64ae5bc421ac337e063c36
leewash3 <95656577+leewash3@users.noreply.github.com>
2022-01-20 08:02:51 -0800

    Add files via upload

---
+++ Project1/force-app/main/default/lwc/products/products.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class Products extends LightningElement {
+    name = 'MacBook Air 13 Inch ';
+    description = 'M1 Chip, 8GB RAM, 256GB SSD';
+    category = 'Laptop';
+    material = 'Aluminum';
+    price = '$999.00';
+    pictureUrl = 'https://www.bing.com/images/search?view=detailV2&ccid=orACsNch&id=8035A34E536F3703FDBB41762E554051B81CE224&thid=OIP.orACsNchufELk_dPISZEdgHaFs&mediaurl=https://luxodeals.com/wp-content/uploads/2021/01/Apple-MacBook-Air-2020-Apple-M1-Chip-8GB-RAM-256GB-SSD-13.3-Inch8.jpg&cdnurl=https://th.bing.com/th/id/R.a2b002b0d721b9f10b93f74f21264476?rik=JOIcuFFAVS52QQ&pid=ImgRaw&r=0&exph=1536&expw=2000&q=MacBook+Air+13+Inch+-+M1+Chip,+8GB+RAM,+256GB+SSD&simid=607997692337590557&form=IRPRST&ck=E4AA0CFDD22023D50C5DA7F150A0D074&selectedindex=0&ajaxhist=0&ajaxserp=0&vt=0&sim=11&adlt=demote&shtp=GetUrl&shid=06c948aa-dc8f-4ed6-b0e7-fad692dea13f&shtk=QXBwbGUgTWFjQm9vayBBaXIgMjAyMCBBcHBsZSBNMSBDaGlwLCA4R0IgUkFNLCAyNTZHQiBTU0QsIDEzLjMgSW5jaCAtIGluIEdvbGQgb3IgR3JleSDCozkxNC44OSA4JSAuLi4%3D&shdk=Rm91bmQgb24gQmluZyBmcm9tIGx1eG9kZWFscy5jb20%3D&shhk=jklxVEg9Kt2KRtpK%2BzQzZlpQJGieXvX93b7xuwGvbD4%3D&shth=OIP.orACsNchufELk_dPISZEdgHaFs';
+}
+}
